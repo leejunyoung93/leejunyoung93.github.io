de3076605dcca627606d8bcc2e8b9affe4080eb3
leejunyoung93 <ab12b1125@gmail.com>
2018-01-25 22:55:13 +0900

    'home'

---
+++ js/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {      
+    $('.menu').click(function(){
+       $(this).toggleClass( "menu_open" );
+       $('.menu_overlay').toggleClass( "open" );
+       $('header').toggleClass( "showmenu" );
+    }); 
+ });
+ 
