b8d4a036373f2ec79b5fb8b88e91554b48916ab6
leejunyoung93 <ab12b1125@gmail.com>
2018-02-04 14:20:40 +0900

    '홈'

--- js/common.js
+++ js/common.js
@@ -11,43 +11,94 @@ function $(selector){
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-    tPage();
+	// tPage();
+	Slider();
 });
 
-/*** 함수선언식은 호이스팅에 영향을 받고 함수표현식은 호이스팅에 영향을 받지 않는다.
-함수표현식은 클로저나 콜백으로 사용할 때 유용하게 쓰인다. ***/
-function tPage(){ 
+// function tPage(){ 
 
-    setTimeout(function(){
-        $tpage.classList.add('tpage_hidden')
-    },1300);    
+//     setTimeout(function(){
+//         $('.tpage').classList.remove('act')
+//     },1300);
 
-    $tpage =  $('.tpage')
+//     $tpage =  $('.tpage')
 
-    /*** 고치기 ***/
-    var tpage_current = $('h2').innerHTML;
-    $tpage.classList.add('tpage_'+ tpage_current +'_out')
+//     /*** 고치기 ***/
+//     var tpage_current = $('h2').innerHTML;
+//     $tpage.classList.add('tpage_'+ tpage_current +'_out')
 
-    /*** 고치기 ***/
-    $header = $('header');
-    if(tpage_current !== 'intro' ){
-        $header.classList.remove('header_hidden')
-    }else{
-        $header.classList.add('header_hidden')
-    }
+//     /*** 고치기 ***/
+//     // 요거 js 에서 처리하지 않고 jekyll에서 하는거 알려줄게.
+//     //$header = $('header');
+//     //if(tpage_current !== 'intro' ){
+//     //    $header.classList.remove('header_hidden')
+//     //}else{
+//     //    $header.classList.add('header_hidden')
+//     //}
 
-    var $tpage_link = $('.tpage_link');
-         /*** 고치기 ***/
-        for (var i = 0; i < $tpage_link.length; i++) {
-            $tpage_link[i].addEventListener('click',function(evt){
-            evt.preventDefault();
-            link_location = this.getAttribute('href');
-            setTimeout(function(){
-                window.location = link_location
-            },2000);
-            var tpage_target = this.getAttribute('data');
-            $tpage.classList.remove('tpage_hidden','tpage_'+ tpage_current +'_out')
-            $tpage.classList.add('tpage_'+ tpage_target +'_in')
-        })
-    } 
-}
+//     var $tpage_link = $('.tpage_link');
+//          /*** 고치기 ***/
+//         for (var i = 0; i < $tpage_link.length; i++) {
+//             $tpage_link[i].addEventListener('click',function(evt){
+//             evt.preventDefault();
+//             link_location = this.getAttribute('href');
+//             setTimeout(function(){
+//                 window.location = link_location
+//             },2000);
+//             var tpage_target = this.getAttribute('data-link');
+//             $tpage.classList.remove('tpage_hidden','tpage_'+ tpage_current +'_out')
+//             $tpage.classList.add('tpage_'+ tpage_target +'_in')
+//         })
+//     } 
+// }
+
+
+function Slider() {	
+	var	$slide = $('.slide'),
+		$item = $('.slide ul.list li.item'),
+		$prevbtn = $('.slide .slide_nav button.prev'),
+		$nextbtn = $('.slide .slide_nav button.next'),
+		autoSlide = true,
+		current = 0
+		
+		$('.slide span.length').innerHTML = "/ " + $item.length;
+
+	$item[current].classList.add('current');
+	if ($item.length > 1){
+		$item[$item.length-1].classList.add('item_prev');
+	}
+
+	var control = function(dir) {
+		if (dir === 'next') {
+			current = current < $item.length-1 ? current + 1 : 0;
+		} else {
+			current = current > 0 ? current - 1 : $item.length-1;
+		}
+
+		var	nextItem = current < $item.length-1 ? current + 1 : 0,
+			prevItem = current > 0 ? current - 1 : $item.length-1;
+
+		$item[current].className = "item current";
+		$item[prevItem].className = "item item_prev";
+		$item[nextItem].className = "item";
+	}
+
+	setInterval(function() {
+		if(autoSlide)control('next');
+	},5000);
+
+	$slide.addEventListener('mouseenter',function(){
+		autoSlide = false;
+	})
+	$slide.addEventListener('mouseleave',function(){
+		autoSlide = true;
+	})
+
+	$prevbtn.addEventListener('click', function() {
+		control('prev');
+	});
+
+	$nextbtn.addEventListener('click', function() {
+		control('next');
+	});
+};
