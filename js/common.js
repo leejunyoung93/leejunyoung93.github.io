13ae83585e058a5da21b7f28ebe3adac441ef381
leejunyoung93 <ab12b1125@gmail.com>
2018-02-09 20:02:01 +0900

    'about'

--- js/common.js
+++ js/common.js
@@ -1,6 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+	tPage();
+	Slider();
+});
+
 function $(selector){
-	var $el = document.querySelectorAll(selector);
-	var count = $el.length;
+	var $el = document.querySelectorAll(selector),
+		count = $el.length;
+
 	if(count > 1){
 		return $el;
 	}else if(count == 1){
@@ -9,91 +15,89 @@ function $(selector){
 		return;
 	}
 }
+function tPage(){ 
+	var $tpage =  $('.tpage');
 
-document.addEventListener('DOMContentLoaded', function(){
-	// tPage();
-	Slider();
+    setTimeout(function(){
+        $tpage.classList.add('hidden')
+	},1800);
+	
+    var tpage_current = $tpage.getAttribute('data-page')
+    $tpage.classList.add('tpage-'+ tpage_current,'out')
+
+	$tpage_link = $('.link');
+        for (var i = 0; i < $tpage_link.length; i++) {
+            $tpage_link[i].addEventListener('click',function(evt){
+            evt.preventDefault();
+            link_location = this.getAttribute('href');
+            setTimeout(function(){
+                window.location = link_location
+            },2000);
+            var tpage_target = this.getAttribute('data-link');
+            $tpage.classList.remove('hidden','tpage-'+ tpage_current,'out')
+            $tpage.classList.add('tpage-'+ tpage_target,'in')
+        })
+    } 
+}	
+document.addEventListener('scroll',function(){
+	if(window.scrollY > 0){
+		$('header').classList.add('bg')
+	}else{
+		$('header').classList.remove('bg')
+	}
 });
 
-// function tPage(){ 
+$('.menu-btn').addEventListener('click',function(){
+	$('.nav-mobile').classList.toggle('open');
+})
 
-//     setTimeout(function(){
-//         $('.tpage').classList.remove('act')
-//     },1300);
-
-//     $tpage =  $('.tpage')
-
-//     /*** 고치기 ***/
-//     var tpage_current = $('h2').innerHTML;
-//     $tpage.classList.add('tpage_'+ tpage_current +'_out')
-
-//     /*** 고치기 ***/
-//     // 요거 js 에서 처리하지 않고 jekyll에서 하는거 알려줄게.
-//     //$header = $('header');
-//     //if(tpage_current !== 'intro' ){
-//     //    $header.classList.remove('header_hidden')
-//     //}else{
-//     //    $header.classList.add('header_hidden')
-//     //}
-
-//     var $tpage_link = $('.tpage_link');
-//          /*** 고치기 ***/
-//         for (var i = 0; i < $tpage_link.length; i++) {
-//             $tpage_link[i].addEventListener('click',function(evt){
-//             evt.preventDefault();
-//             link_location = this.getAttribute('href');
-//             setTimeout(function(){
-//                 window.location = link_location
-//             },2000);
-//             var tpage_target = this.getAttribute('data-link');
-//             $tpage.classList.remove('tpage_hidden','tpage_'+ tpage_current +'_out')
-//             $tpage.classList.add('tpage_'+ tpage_target +'_in')
-//         })
-//     } 
-// }
-
-
-
-
-function Slider() {	
-	var	$slide = $('.slide'),
-		$item = $('.slide ul.list li.item'),
-		$prevbtn = $('.slide .slide_nav button.prev'),
-		$nextbtn = $('.slide .slide_nav button.next'),
-		autoSlide = true,
+function Slider() {
+	var	$slider = $('.slider'),
+		$item = $('.slide'),
+		$prevbtn = $('.slider-nav .prev'),
+		$nextbtn = $('.slider-nav .next'),
+		slideLength = $item.length,
+		autoSlider = true,
 		current = 0;
-		
-		$('span.length').innerHTML = "/ " + $item.length;
 
+	$('.count-length').innerHTML = '/ ' + slideLength;
 	$item[current].classList.add('current');
-	if ($item.length > 1){
-		$item[$item.length-1].classList.add('item_prev');
+
+	if (slideLength > 1){
+		$item[slideLength-1].classList.add('prev');
 	}
 
 	var control = function(dir) {
 		if (dir === 'next') {
-			current = current < $item.length-1 ? current + 1 : 0;
+			current = current < slideLength-1 ? current + 1 : 0;
 		} else {
-			current = current > 0 ? current - 1 : $item.length-1;
+			current = current > 0 ? current - 1 : slideLength-1;
 		}
 
-		var	nextItem = current < $item.length-1 ? current + 1 : 0,
-			prevItem = current > 0 ? current - 1 : $item.length-1;
+		var	nextItem = current < slideLength-1 ? current + 1 : 0,
+			prevItem = current > 0 ? current - 1 : slideLength-1;
 
-		$item[current].className = "item current";
-		$item[prevItem].className = "item item_prev";
-		$item[nextItem].className = "item";
+		$item[current].classList.add('current');
+		$item[current].classList.remove('prev');
+		$item[prevItem].classList.add('prev');
+		$item[prevItem].classList.remove('current');
+		$item[nextItem].classList.remove('prev');
+		$item[nextItem].classList.remove('current');
 	}
 
 	setInterval(function() {
-		if(autoSlide)control('next');
+		if(autoSlider){
+			control('next');
+		}
 	},5000);
 
-	$slide.addEventListener('mouseenter',function(){
-		autoSlide = false;
+	$slider.addEventListener('mouseenter',function(){
+		autoSlider = false;
+		return;
 	})
-	$slide.addEventListener('mouseleave',function(){
-		autoSlide = true;
+	$slider.addEventListener('mouseleave',function(){
+		autoSlider = true;
+		return;
 	})
 
 	$prevbtn.addEventListener('click', function() {
@@ -104,3 +108,22 @@ function Slider() {
 		control('next');
 	});
 };
+
+
+var position = new naver.maps.LatLng(37.3919100, 126.9845080);
+var map = new naver.maps.Map('map', {
+    center: position,
+    zoom: 10
+});
+var markerOptions = {
+    position: position,
+    map: map,
+    icon: '../img/pin.png'
+};
+var marker = new naver.maps.Marker(markerOptions);
+
+
+
+
+
+
